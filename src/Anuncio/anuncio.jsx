a60cab3787697f31912c4d6f76e67426d2e2c937
unknown <Luis.cordero.cg@gmail.com>
2023-04-12 19:54:57 -0600

    actualizacion

--- src/Anuncio/anuncio.jsx
+++ src/Anuncio/anuncio.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./anuncio.css";
-import { Helmet } from "react-helmet";
+
 
 function Anuncio(props){
 
     return (
         <div className="publicidad">
-            <p>imagen publicitaria</p>
+            <p>{props.titul}</p>
             <img className="impubli" src={props.imgen} />
         </div>
     )
